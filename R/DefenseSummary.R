5bc5aceb0fc9428f7e2690c20d3543ad9e685efe
dpwynne <dpwynne@gmail.com>
2020-06-29 10:36:25 -0700

    added the .data

--- R/DefenseSummary.R
+++ R/DefenseSummary.R
@@ -15,6 +15,7 @@
 #' @importFrom dplyr select
 #' @importFrom dplyr mutate
 #' @importFrom dplyr lead
+#' @importFrom rlang .data
 #'
 #' @export
 DigSummary <- function(plays, ...){
@@ -40,22 +41,20 @@ DigSummary <- function(plays, ...){
   # These plays have the same critera as CRT but the attack must be a kill
   # In attacks, the point can be recorded immediately, after a reception error, or after a poor reception that leads to a setting error. Here, only the first two options are allowed.
   CNT_1 <- which(rownames(plays) %in% (attempt_ids+1) & plays$skill == "Attack" & plays$evaluation_code == "#") - 1
-  CNT_1 <- CNT_1[rank(CNT_1) %in% which(plays[CNT_1,]$team == plays[CNT_1 + 1,]$team)]
+  CNT_1 <- CNT_1[rank(CNT_1) %in% which(plays$team[CNT_1] == plays$team[CNT_1 + 1])]
   CNT_2 <- which(rownames(plays) %in% (attempt_ids+2) & plays$skill == "Attack" & plays$evaluation_code == "#") - 2
-  CNT_2 <- CNT_2[rank(CNT_2) %in% which(plays[CNT_2,]$team == plays[CNT_2 + 2,]$team)]
+  CNT_2 <- CNT_2[rank(CNT_2) %in% which(plays$team[CNT_2] == plays$team[CNT_2 + 2])]
 
   CNT_ids <- c(CNT_1, CNT_2) %>% unique()
   CNT_ids <- which(rownames(plays) %in% (CNT_ids + 3) & (plays$evaluation_code != "!" | is.na(plays$evaluation_code))) - 3
 
-
-
   CRT <- plays[CRT_ids,] %>%
     group_by(...) %>%
-    summarize(CRT = ~ n())
+    summarize(CRT = n())
 
   CNT <- plays[CNT_ids,] %>%
     group_by(...) %>%
-    summarize(CNT = ~ n())
+    summarize(CNT = n())
 
 
   ### Produce output
@@ -65,15 +64,17 @@ DigSummary <- function(plays, ...){
 
   output <- defense %>%
     group_by(...) %>%
-    summarize(Touched = ~ n(),
-              Digs = ~ sum(evaluation_code != "=")) %>%
+    summarize(Touched = n(),
+              Digs = sum(.data$evaluation_code != "=")) %>%
     left_join(CRT) %>%
     left_join(CNT) %>%
-    mutate(`T%` = ~ Touched/sum(Touched),
-           `Digs%` = ~ Digs/Touched,
-           `CRT%` = ~ CRT/Touched,
-           `CNT%` = ~ CNT/Touched) %>%
+    mutate(`T%` = .data$Touched/sum(.data$Touched),
+           `Digs%` = .data$Digs/.data$Touched,
+           `CRT%` = .data$CRT/.data$Touched,
+           `CNT%` = .data$CNT/.data$Touched) %>%
     select(..., Touched, `T%`, Digs, `Digs%`, CRT, `CRT%`, CNT, `CNT%`)
+
+  # Note: the select function may not work properly
 
   return(output)
 }
