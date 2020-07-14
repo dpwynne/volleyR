4fb6f81c1acaf1fd87c8639a2882197127426644
dpwynne <dpwynne@gmail.com>
2020-07-14 13:26:12 -0700

    add documentation

--- R/FirstBallAttacks.R
+++ R/FirstBallAttacks.R
@@ -1,11 +1,26 @@
+#' FirstBallAttacks
+#'
+#' Joins serve receptions in a DataVolley play-by-play file with the resulting attack (if any)
+#'
+#' @param plays a dv_plays object or data frame containing play-by-play data
+#'
+#' @return A grouped data frame
+#'
+#' @importFrom magrittr %>%
+#' @importFrom dplyr slice
+#' @importFrom dplyr select
+#' @importFrom dplyr left_join
+#' @importFrom rlang .data
+#'
+#' @export
 FirstBallAttacks <- function(plays){
   reception_ids <- which(plays$skill == "Reception")
   FB_attack_ids <- which(plays$skill == "Attack" & plays$phase == "Reception") # Row numbers of all attacks
 
   receptions <- plays %>% slice(reception_ids)
-  FB_attacks <- plays %>% slice(FB_attack_ids) %>% select(point_id,
-                                                          attack_team = team,
-                                                          attack_result = evaluation)
+  FB_attacks <- plays %>% slice(FB_attack_ids) %>% select(.data$point_id,
+                                                          attack_team = .data$team,
+                                                          attack_result = .data$evaluation)
   reception_attacks <- left_join(receptions, FB_attacks, by = "point_id")
 
   return(reception_attacks)
